e202cf19074cf77e218609d523bc98eeb640bc52
Saroj Shrestha <shresthasaroj7777@gmail.com>
2026-01-14 18:31:18 +0545

    Serializer and Deserializer

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,2 +1,17 @@
 from django.shortcuts import render
 from . models import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import CategorySerializers
+
+@api_view(['GET', 'POST'])
+def category(request):
+    if request.method == 'GET':
+        category_1 = Category.objects.all()
+        serializer = CategorySerializers(category_1, many = True)
+        return Response (serializer.data)
+    elif request.method == 'POST':
+        serializer = CategorySerializers(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response (serializer.data)
--- restaurant_main/urls.py
+++ restaurant_main/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('restaurant.urls')),
+    path('api/v1/', include('restaurant.urls')),
 ]
 
 if settings.DEBUG:
